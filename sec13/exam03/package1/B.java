712f50eeb39ea11054236dfffbba27dea453f935
Harris-Oak <ohshrs0413@naver.com>
2025-11-26 21:48:57 +0900

    객체지향프로그래밍 12주차 과제 제출
    객체지향프로그래밍 12주차 과제 제출입니다.

---
+++ sec13/exam03/package1/B.java
@@ -0,0 +1,11 @@
+package ch06.sec13.exam03.package1;
+
+public class B {
+    public void use() {
+        A a = new A();
+        a.field1 = 10;
+        a.field2 = 20;
+        a.method1();
+        a.method2();
+    }
+}
